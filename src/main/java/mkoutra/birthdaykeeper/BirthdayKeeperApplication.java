b4f4caca69a6a10a8995079454e5e9af651b41a9
mkoutra <koutra98@hotmail.com>
2024-11-05 00:51:48 +0200

    Initial Commit

---
+++ src/main/java/mkoutra/birthdaykeeper/BirthdayKeeperApplication.java
@@ -0,0 +1,13 @@
+package mkoutra.birthdaykeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdayKeeperApplication.class, args);
+    }
+
+}
