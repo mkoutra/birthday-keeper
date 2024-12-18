81d427f1efc17928b5b3cb4b50038271ce6988dc
mkoutra <koutra98@hotmail.com>
2024-12-18 17:23:36 +0200

    Improve User Service & Admin Controller
    - Add the following methods to User Service layer:
      - getAllUsers()
      - getPaginatedUsers()
      - deleteUser()
      - overrideUserPassword()
      - changeUserPassword()
    - Create UserChangePasswordDTO.
    - Create AdminRestController with the following methods:
        - getAllUsers()
        - getPaginatedUsers()
        - deleteUser()

--- src/main/java/mkoutra/birthdaykeeper/service/UserService.java
+++ src/main/java/mkoutra/birthdaykeeper/service/UserService.java
@@ -3,6 +3,8 @@ package mkoutra.birthdaykeeper.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import mkoutra.birthdaykeeper.core.exceptions.EntityAlreadyExistsException;
+import mkoutra.birthdaykeeper.core.exceptions.EntityNotFoundException;
+import mkoutra.birthdaykeeper.dto.userDTOs.UserChangePasswordDTO;
 import mkoutra.birthdaykeeper.dto.userDTOs.UserInsertDTO;
 import mkoutra.birthdaykeeper.dto.userDTOs.UserReadOnlyDTO;
 import mkoutra.birthdaykeeper.mapper.Mapper;
@@ -10,7 +12,14 @@ import mkoutra.birthdaykeeper.model.User;
 import mkoutra.birthdaykeeper.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +28,24 @@ public class UserService implements IUserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final Mapper mapper;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public List<UserReadOnlyDTO> getAllUsers() {
+        return userRepository
+                .findAll()
+                .stream()
+                .map(mapper::mapToUserReadOnlyDTO)
+                .toList();
+    }
+
+    @Override
+    public Page<UserReadOnlyDTO> getPaginatedUsers(int pageNo, int size) {
+        String defaultSorting = "id";
+        Pageable pageable = PageRequest.of(pageNo, size, Sort.by(defaultSorting).ascending());
+        return userRepository.findAll(pageable).map(mapper::mapToUserReadOnlyDTO);
+    }
 
     @Override
     @Transactional(rollbackOn = Exception.class)
@@ -38,5 +65,57 @@ public class UserService implements IUserService {
             LOGGER.error("User Error: User with username {} does not exist.", userInsertDTO.getUsername());
             throw e;
         }
+    }
+
+    @Override
+    @Transactional(rollbackOn = Exception.class)
+    public UserReadOnlyDTO deleteUser(Long id) throws EntityNotFoundException {
+        try {
+            UserReadOnlyDTO userToDeleteDTO = userRepository
+                    .findById(id)
+                    .map(mapper::mapToUserReadOnlyDTO)
+                    .orElseThrow(() -> new EntityNotFoundException("User",
+                            "User with id " + id + " was not found"));
+            userRepository.deleteById(id);
+            LOGGER.info("User with id: {} and all their friends deleted successfully.", id);
+            return userToDeleteDTO;
+        } catch(EntityNotFoundException e) {
+            LOGGER.error("User Error: User with id {} does not exist.", id);
+            throw  e;
+        }
+    }
+
+    /**
+     * Updates the password for the user with the specified ID.
+     *
+     * @param id                        The ID of the user whose password is to be updated.
+     * @param newPassword               The new password to set for the user.
+     * @return                          A DTO containing the updated user's information.
+     * @throws EntityNotFoundException  If no user with the specified ID exists.
+     */
+    @Override
+    @Transactional(rollbackOn = Exception.class)
+    public UserReadOnlyDTO overrideUserPassword(Long id, String newPassword) throws EntityNotFoundException {
+        try {
+            User user = userRepository
+                    .findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("User",
+                            "User with id " + id + " was not found"));
+
+            user.setPassword(passwordEncoder.encode(newPassword));
+            User userWithNewPassword = userRepository.save(user);
+
+            return mapper.mapToUserReadOnlyDTO(userWithNewPassword);
+        } catch (EntityNotFoundException e) {
+            LOGGER.error("User Error: User with id {} does not exist.", id);
+            throw  e;
+        }
+    }
+
+    // TODO
+    @Override
+    @Transactional(rollbackOn = Exception.class)
+    public UserReadOnlyDTO changeUserPassword(UserChangePasswordDTO changePasswordDTO) throws EntityNotFoundException {
+        return null;
     }
 }
